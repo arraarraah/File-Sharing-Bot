48f0fb9b8abdc1eab7d65ea313f83b8332416eb2
arraarraah <risaameliaputriagung2022@gmail.com>
2025-01-27 21:57:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -56,11 +56,11 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 
 
-USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
+USER_REPLY_TEXT = "❌Jangan Kirimkan Saya Pesan Secara Langsung Saya Hanya Bot Berbagi File !"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nSaya dapat menyimpan file pribadi di saluran tertentu dan pengguna lain dapat mengaksesnya melalui tautan khusus.")
 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>Anda Perlu Bergabung Di Channel/Grup Saya Untuk Menggunakan Saya\n\nMohon Bergabung dengan Channel</b>")
 
 
 
